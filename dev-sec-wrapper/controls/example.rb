8afdda0dcac11e41a57d981e41f2d2361c1912b6
Ian Miell <ian.miell@gmail.com>
2017-12-21 16:17:17 +0000

    others

--- dev-sec-wrapper/controls/example.rb
+++ dev-sec-wrapper/controls/example.rb
@@ -20,3 +20,11 @@ control 'tmp-1.0' do                        # A unique ID for this control
 end
 
 include_controls 'linux-baseline'
+include_controls 'php-baseline'
+include_controls 'postgres-baseline'
+include_controls 'ssh-baseline'
+include_controls 'ssl-baseline'
+include_controls 'linux-patch-baseline'
+include_controls 'cis-linux-benchmark'
+include_controls 'apache-benchmark'
+include_controls 'cve-2016-5195'
